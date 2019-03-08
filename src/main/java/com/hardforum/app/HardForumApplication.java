fcfcc3d04adff29ab5fb1e4e2302ad66e176f3f4
unknown <kevin.butikofer@he-arc.ch>
2019-03-08 08:57:58 +0100

    init spring project

---
+++ src/main/java/com/hardforum/app/HardForumApplication.java
@@ -0,0 +1,13 @@
+package com.hardforum.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardForumApplication.class, args);
+	}
+
+}
